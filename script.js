5e2ea8a4c8f7c2af3171f42858bfdf115bbc6e3a
Zekiie <35592120+Zekiie@users.noreply.github.com>
2018-02-06 11:58:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ var secondHand = document.querySelector(".second-hand"),
 function setDate() {
     var now = new Date();
     console.log(now);
+    
     // get seconds
     var seconds = now.getSeconds();
     var secondsDegrees = (seconds / 60) * 360 + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
     //get minutes
     var minutes = now.getMinutes();
     var minutesDegrees = (minutes / 60) * 360 + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    
     //get hours
     var hours = now.getHours();
     var hoursDegrees = (hours / 12) * 360 + 90;
